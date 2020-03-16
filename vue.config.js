fa7f98cf8835c68f460d865b1c29db3d99bc091e
Enar <enmust@ttu.ee>
2020-03-16 12:07:07 +0200

    added sitemap

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        "https://turvas.geoloogia.info/",
+        "https://turvas.geoloogia.info/#/",
+        "https://turvas.geoloogia.info/#/area",
+        "https://turvas.geoloogia.info/#/site",
+        "https://turvas.geoloogia.info/#/sample",
+        "https://turvas.geoloogia.info/#/about"
+      ]
+    }
+  }
+};
