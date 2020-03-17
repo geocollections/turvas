4827d6cf09b7ced98df1190defb310ec53aadf74
Enar <enmust@ttu.ee>
2020-03-17 12:13:50 +0200

    small updates

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,10 @@ module.exports = {
     sitemap: {
       urls: [
         "https://turvas.geoloogia.info/",
-        "https://turvas.geoloogia.info/#/",
-        "https://turvas.geoloogia.info/#/area",
-        "https://turvas.geoloogia.info/#/site",
-        "https://turvas.geoloogia.info/#/sample",
-        "https://turvas.geoloogia.info/#/about"
+        "https://turvas.geoloogia.info/area",
+        "https://turvas.geoloogia.info/site",
+        "https://turvas.geoloogia.info/sample",
+        "https://turvas.geoloogia.info/about"
       ]
     }
   }
