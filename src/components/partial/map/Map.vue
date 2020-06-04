9b774715b84e2310e966f48aff78db2061313c84
Enar <enmust@ttu.ee>
2020-06-04 11:24:24 +0300

    title and kik logo z-index fix

--- src/components/partial/map/Map.vue
+++ src/components/partial/map/Map.vue
@@ -36,15 +36,28 @@
       href="https://kik.ee"
       target="KikWindow"
       title="Keskkonnainvesteeringute keskus"
+      v-if="$route.name === 'FrontPage'"
     >
       <img
-        v-if="$route.name === 'FrontPage'"
         src="https://www.kik.ee/sites/default/files/kik_est_logo__2.png"
         class="kik-logo"
         alt="kik logo"
         :class="$vuetify.breakpoint.smAndDown ? 'kik-logo-sm' : ''"
       />
     </a>
+
+    <div
+      v-if="$route.name === 'FrontPage'"
+      class="app-title"
+      :class="{
+        'app-title-xs': $vuetify.breakpoint.xsOnly,
+        'app-title-sm': $vuetify.breakpoint.smOnly,
+        'app-title-md': $vuetify.breakpoint.mdOnly,
+        'app-title-lg': $vuetify.breakpoint.lgAndUp
+      }"
+    >
+      Eesti turbauuringute andmebaas
+    </div>
   </div>
 </template>
 
@@ -817,7 +830,7 @@ export default {
 
 .kik-logo {
   position: absolute;
-  z-index: 500;
+  z-index: 450;
   width: 150px;
   bottom: 0;
   left: 0;
@@ -827,5 +840,41 @@ export default {
 
 .kik-logo-sm {
   width: 100px;
+}
+
+.app-title {
+  position: absolute;
+  top: 80px;
+  left: 70px;
+  right: 50px;
+  z-index: 450;
+  color: #fff;
+  font-weight: 600;
+  text-shadow: 1px 1px 10px #000;
+}
+
+.app-title-xs {
+  font-size: 1.35em;
+  left: 55px;
+  top: 75px;
+  max-width: 300px;
+}
+
+.app-title-sm {
+  font-size: 2em;
+  left: 60px;
+  top: 75px;
+  max-width: 400px;
+}
+
+.app-title-md {
+  font-size: 2.5em;
+  max-width: 500px;
+  left: 70px;
+}
+
+.app-title-lg {
+  font-size: 3em;
+  max-width: 600px;
 }
 </style>
